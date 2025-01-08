5560417ddd93f57cc8d45aceff61fed51d9e34ad
samandar <samandartoyirov020@gmail.com>
2025-01-08 14:53:56 +0500

    first-commit

---
+++ src/main/java/result/agency/result_agency_intern/ResultAgencyInternApplication.java
@@ -0,0 +1,13 @@
+package result.agency.result_agency_intern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultAgencyInternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResultAgencyInternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/result/agency/result_agency_intern/ResultAgencyInternApplicationTests.java
@@ -0,0 +1,13 @@
+package result.agency.result_agency_intern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultAgencyInternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
